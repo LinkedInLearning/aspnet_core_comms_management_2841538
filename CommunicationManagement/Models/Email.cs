971b8cb57f9a3450fe784997328d476a8aabb102
Ervis Trupja <ervistrupja@outlook.com>
2020-09-13 13:42:03 +0200

    03_05b initial

---
+++ CommunicationManagement/Models/Email.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunicationManagement.Models
+{
+    public class Email
+    {
+        [Key]
+        public int Id { get; set; }
+        public string EmailAddress { get; set; }
+        public string FullName { get; set; }
+        public string SendGridKey { get; set; }
+        public string Status { get; set; }
+    }
+}
