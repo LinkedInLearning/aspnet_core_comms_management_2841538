65b190947bce4f12ac87f86d8b5634f1dc28b845
Ervis Trupja <ervistrupja@outlook.com>
2020-09-13 13:36:55 +0200

    03_03b initial
    03_03b initial code

---
+++ CommunicationManagement/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CommunicationManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunicationManagement.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<Email> Emails { get; set; }
+    }
+}
