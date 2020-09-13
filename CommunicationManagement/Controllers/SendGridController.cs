3f2fa0ac12f958f0e60441cbccb36ce2904a3be5
Ervis Trupja <ervistrupja@outlook.com>
2020-09-13 13:39:09 +0200

    03_03e initial

---
+++ CommunicationManagement/Controllers/SendGridController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommunicationManagement.Data;
+using CommunicationManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommunicationManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SendGridController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public SendGridController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [Route("webhook")]
+        public IActionResult WebHook(List<SendGridEvents> data)
+        {
+            var debug = data;
+            foreach (var _event in data)
+            {
+                var dbEmail = _context.Emails.FirstOrDefault(n => _event.sg_message_id.StartsWith(n.SendGridKey));
+
+                if(dbEmail != null)
+                {
+                    dbEmail.Status = _event.@event;
+                    _context.SaveChanges();
+                }
+            }
+            return Ok(debug);
+        }
+    }
+}
---
+++ CommunicationManagement/Models/SendGridEvents.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunicationManagement.Models
+{
+    public class SendGridEvents
+    {
+        public string email { get; set; }
+        public long timestamp { get; set; }
+        public string @event { get; set; }
+        [JsonProperty("smtp-id")]
+        public string smtp_id { get; set; }
+        public string useragent { get; set; }
+        public string ip { get; set; }
+        public string sg_event_id { get; set; }
+        public string sg_message_id { get; set; }
+        public string reason { get; set; }
+        public string status { get; set; }
+        public string response { get; set; }
+        public string tls { get; set; }
+        public Uri url { get; set; }
+        public string urloffset { get; set; }
+        public string attempt { get; set; }
+        public string category { get; set; }
+        public string type { get; set; }
+    }
+}
