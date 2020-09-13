5aae4117f617509e4376b4e5144b3d10ebeead69
Ervis Trupja <ervistrupja@outlook.com>
2020-09-13 13:24:22 +0200

    02_05b initial

---
+++ CommunicationManagement/Data/Services/EmailService.cs
@@ -0,0 +1,41 @@
+﻿using CommunicationManagement.Data.ViewModels;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunicationManagement.Data.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _configuration;
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public Task<Response> SendSingleEmail(ComposeEmailVM payload)
+        {
+            var apiKey = _configuration.GetSection("SendGrid")["ApiKey"];
+
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("no-reply@dotnethow.net", "Ervis Trupja");
+
+            var subject = payload.Subject;
+            var to = new EmailAddress(payload.Email, $"{payload.FirstName} {payload.LastName}");
+            var textContent = payload.Body;
+            var htmlContent = $"<strong>{payload.Body}</strong>";
+
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, textContent, htmlContent);
+
+            var request = client.SendEmailAsync(msg);
+            request.Wait();
+            var result = request.Result;
+
+            return request;
+        }
+    }
+}
---
+++ CommunicationManagement/Data/Services/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using CommunicationManagement.Data.ViewModels;
+using SendGrid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunicationManagement.Data.Services
+{
+    public interface IEmailService
+    {
+        Task<Response> SendSingleEmail(ComposeEmailVM payload);
+    }
+}
