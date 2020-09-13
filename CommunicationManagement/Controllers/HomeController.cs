dbeabdb2ccf75d003eb25abb52f4c7ac58d192cf
Ervis Trupja <ervistrupja@outlook.com>
2020-09-13 13:15:15 +0200

    initial code commit
    initial code commit

---
+++ CommunicationManagement/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CommunicationManagement.Data.ViewModels;
+
+namespace CommunicationManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Compose()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(ComposeEmailVM composeEmail)
+        {
+            try
+            {
+                return View("Success");
+            }
+            catch (Exception)
+            {
+                return View("Error");
+            }
+        }
+
+    }
+}
