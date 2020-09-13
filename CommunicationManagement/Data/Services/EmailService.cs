8c2628aeb3d0d0803f14fbf132d3e957fb881972
Ervis Trupja <ervistrupja@outlook.com>
2020-09-13 13:20:13 +0200

    initial code commit for 02_03b

---
+++ CommunicationManagement/Data/Services/EmailService.cs
@@ -0,0 +1,25 @@
+﻿using CommunicationManagement.Data.ViewModels;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunicationManagement.Data.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _configuration;
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public Task<Response> SendSingleEmail(ComposeEmailVM payload)
+        {
+            var apiKey = _configuration.GetSection("SendGrid")["ApiKey"];
+            return null;
+        }
+    }
+}
