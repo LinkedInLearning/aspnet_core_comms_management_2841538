3b5c5c6bebe35e307a75b49a6fcc4605ba2caa1f
Ervis Trupja <ervistrupja@outlook.com>
2020-09-13 13:22:18 +0200

    initial 02_03e code

---
+++ CommunicationManagement/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CommunicationManagement.Data.ViewModels;
+using CommunicationManagement.Data.Services;
+
+namespace CommunicationManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IEmailService _service;
+
+        public HomeController(ILogger<HomeController> logger, IEmailService service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Compose()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(ComposeEmailVM composeEmail)
+        {
+            try
+            {
+                _service.SendSingleEmail(composeEmail);
+                return View("Success");
+            }
+            catch (Exception)
+            {
+                return View("Error");
+            }
+        }
+
+    }
+}
